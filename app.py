9f8279f50fc9304b83ec2cd71b314b1bd7f6e42e
Hunter John Larco <hjlarco@gmail.com>
2015-02-13 16:17:27 -0500

    Device Accelerometer Viewer

--- app.py
+++ app.py
@@ -13,7 +13,15 @@ class MainHandler(webapp2.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
 
+class MobileHandler(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'mobile.html')
+    self.response.out.write(template.render(path, template_values))
+
+
 app = webapp2.WSGIApplication([
                 lib.api.Hook,
+                ('/mobile/?', MobileHandler),
                 ('/.*', MainHandler)
               ], debug=True)
